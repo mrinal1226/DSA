9f88c065deb2c866a8761fe06152027ce5fe6853
Mrinal <mrinalsharma1226@gmail.com>
2023-09-05 00:05:20 +0530

    first commit

---
+++ IF ELSE , WHILE LOOP/positivenegative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5;
+    
+    if(a>0)
+    {
+        cout<<"positive no"<<endl;
+    }
+    else
+    {
+        cout<<"negative no";
+    }
+}
+
