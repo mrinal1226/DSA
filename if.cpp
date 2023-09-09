ebd65a546b16345a93b47c599615c6a42fd7f008
mrinal1226 <123577246+mrinal1226@users.noreply.github.com>
2023-09-10 00:39:11 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a no. "<<endl;
+    cin>>a;
+if(a>0){
+    cout<<"a positive no"<<endl;
+}
+else{
+    cout<<"its a negative number!"<<endl;
+}
+return 0;
+}
