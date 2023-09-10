f7f2c29e83dfe2289c0e765efa15cd6c9cda10ac
mrinal1226 <123577246+mrinal1226@users.noreply.github.com>
2023-09-10 23:11:34 +0530

    Add files via upload

---
+++ sumofevennowhile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=0;
+    int sum=0;
+    while(i<=n){
+        sum=sum+i;
+        i=i+2;
+    }
+    cout<<"sum of even no"<<sum<<endl;
+}
