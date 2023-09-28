097077cf0fafa9a5a9fbc6fc5eca763901b54340
mrinal1226 <123577246+mrinal1226@users.noreply.github.com>
2023-09-29 02:00:46 +0530

    Add files via upload

---
+++ increment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=6;
+    cout<<"(i++)"<<i++<<endl;
+     cout<<"(++i)"<<i++<<endl;
+      cout<<"(i--)"<<i++<<endl;
+       cout<<"(--i)"<<i++<<endl;
+}
